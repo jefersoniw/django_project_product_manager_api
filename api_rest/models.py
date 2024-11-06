9afb888c22b4fae7deda6c7a4a9846c925ba7b5e
Jeferson Chagas Silva <jeferson.chagas@salvador.ba.gov.br>
2024-11-06 11:50:44 -0300

    feat: Ajustes no models, criando relacionamento entre client e product e fazendo migração para o banco de dados.

--- api_rest/models.py
+++ api_rest/models.py
@@ -1,40 +1,51 @@
 from django.db import models
 
 # Create your models here.
+
+#model base para definições de criação e atualizaçãoi de registros
+class Base(models.Model):
+  created_at = models.DateTimeField(auto_now=True)
+  updated_at = models.DateTimeField(auto_now_add=True)
+  
+  class Meta:
+    abstract = True
+
 #model User
-class User(models.Model):
+class User(Base):
   name = models.CharField(max_length=100)
   email = models.EmailField(unique=True)
   password = models.CharField(max_length=15)
+  
+  class Meta:
+    verbose_name = 'User'
+    verbose_name_plural = 'Users'
   
   def __str__(self):
     return f'Name: {self.name} | Email: {self.email}'
   
 #model Client
-class Client(models.Model):
-  SEX_CHOICES = [
-    ('M', 'Masculino')
-    ('F', 'Feminino')
-    ('O', 'Outros')
-  ]
-  
+class Client(Base):  
   name = models.CharField(max_length=100)
   cpf = models.CharField(max_length=11, unique=True)
   address = models.CharField(max_length=255)
-  sex = models.CharField(max_length=1, choices=SEX_CHOICES)
-  created_at = models.DateTimeField(auto_now=True)
-  updated_at = models.DateTimeField(auto_now_add=True)
+  sex = models.CharField(max_length=1)
+  
+  class Meta:
+    verbose_name = 'Client'
+    verbose_name_plural = 'Clients'
   
   def __str__(self):
     return f'Name: {self.name} | CPF: {self.cpf}'
   
 #model Product
-class Product(models.Model):  
+class Product(Base):  
   name = models.CharField(max_length=100)
   price = models.DecimalField(max_digits=10, decimal_places=2)
-  client = models.models.ForeignKey(Client, on_delete=models.CASCADE, related_name="products")
-  created_at = models.DateTimeField(auto_now=True)
-  updated_at = models.DateTimeField(auto_now_add=True)
+  client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="products")
+  
+  class Meta:
+    verbose_name = 'Product'
+    verbose_name_plural = 'Products'
   
   def __str__(self):
     return f'Product: {self.name}'
