29731c481ea57bb5f17a62df152eeef0dce268b7
Jeferson Chagas Silva <jeferson.chagas@salvador.ba.gov.br>
2024-11-08 13:16:46 -0300

    feat: Implementando modulo de products.

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -14,5 +14,11 @@ urlpatterns = [
     path('clients/<int:id>', views.getClient, name='get_client'),
     path('clients/create', views.createClient, name='create_client'),
     path('clients/update/<int:id>', views.updateClient, name='update_client'),
-    path('clients/delete/<int:id>', views.deleteClient, name='delete_client')
+    path('clients/delete/<int:id>', views.deleteClient, name='delete_client'),
+    
+    path('products', views.getProducts, name='get_all_products'),
+    path('products/<int:id>', views.getProduct, name='get_product'),
+    path('products/create', views.createProduct, name='create_product'),
+    path('products/update/<int:id>', views.updateProduct, name='update_product'),
+    path('products/delete/<int:id>', views.deleteProduct, name='delete_product')
 ]
